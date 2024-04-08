5bfa41a48a17ea562c2b9e4dc382e649e22024a9
Navadeep-Reddy <vnavadeepreddysatti@gmail.com>
2024-04-08 18:48:19 +0530

    Added the code for facial recognition without storing the encodings

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Face_Recog
 A simple program to run quick facial recognition
+
+#How to use
+Place all photos inside the Faces directory to obtain the encoding
+Provide the path to the new photo to run facial recog as command line argument
---
+++ face_rec.py
@@ -0,0 +1,55 @@
+import face_recognition as fr
+import glob
+import os
+
+#List for paths of known faces
+face_path = []
+
+#List for known face encodings 
+encoding_store = []
+
+#List for names
+name_store = []
+
+#List for possible file types
+file_types = ["*.jpg", "*.png", "*.svg", "*.gif", "*.webp"]
+
+location = "/home/navadeep/Downloads/Faces"
+
+#Get path of photos of all recorded people from the stored location
+for i in file_types:
+    face_path.extend(glob.glob(os.path.join(location, i)))
+    
+for p in face_path:
+    #Load face
+    face = fr.load_image_file(p)
+    #Get encoding of the first face found
+    encoding = fr.face_encodings(face)[0]
+
+    #Store the encoding of the face
+    encoding_store.append(encoding)
+
+    #Store the name of the face
+    name_store.append(p[len(location) + 1: -4:])
+
+#Loading unknown faces and getting its encodings
+unknown_face = fr.load_image_file("/home/navadeep/Downloads/aku.png")
+unknown_encoding = fr.face_encodings(unknown_face)
+
+#Looping through the encodings found and checking if they match
+for single_encoding in unknown_encoding:
+
+    #Boolean list after comparing unknown_encoding to every encoding in data set
+    match_list = fr.compare_faces(encoding_store, single_encoding)
+
+    if True in match_list:
+        first_match_index = match_list.index(True)
+        print(name_store[first_match_index])
+
+    else:
+        print("No matches found")
+
+
+
+
+
